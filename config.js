0fae2865121a8b45905afa48781a307261160c0c
anstoreofc <132449188+anstoreofc@users.noreply.github.com>
2023-06-29 14:19:51 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,52 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.apikey = '-' //https://api.lolhuman.xyz
+global.rosekey = '-' //https://api.itsrose.life
+//—————「 Set Nama Bot & Own 」—————//
+global.namabot = 'itsuka botz'
+global.namaowner = 'itsuka お-ぎ'
+
+//—————「 Setting Owner 」—————//
+global.owner = '6285692195658'
+global.ownernomer = ["6285692195658"]
+global.premium = ['6285692195658']
+
+//—————「 Set Wm 」—————//
+global.packname = 'itsuka'
+global.author = 'itsuka お-ぎ'
+global.prefa = ['#', '.']
+global.sp = '•'
+
+//—————「 Set Message 」—————//
+global.mess = {
+    done: 'Done ✅',
+    admin: 'Feature Only for _*Admin Group*_',
+    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin Group !',
+    owner: 'Feature Only for _*owner*_',
+    group: 'Feature Only for _*Group Chat*_',
+    private: 'Feature Only for _*Admin Group*_',
+    wait: 'Wait a Moment, for Process',
+    endLimit: 'Your imit has run out, Wait at 12 at night',
+    error: '*!!!Feature Error!!!*',
+}
+
+//—————「 Set Limit 」—————//
+global.limitawal = {
+    premium: "Infinity",
+    free: 50,
+}
+
+//—————「 Set Image 」—————//
+global.imageurl = 'https://telegra.ph/file/b8a7b1262c5c962b8fe61.jpg'
+global.isLink = `https://youtube.com/@itsukabotz`
+global.thumb = fs.readFileSync('./media/thumb.jpg')
+
+//—————「 Batas Akhir 」—————//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
